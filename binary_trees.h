559e66b38f8f2f58b149bf5115681515f01c387c
Victor Zuluaga <zrvictor@outlook.com>
2020-11-24 19:03:09 -0500

    prototypes Binary Trees

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -33,5 +34,9 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+size_t binary_tree_height2(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
