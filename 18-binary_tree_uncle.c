9ee07d1264e5cab64d3d8dc004a7ad864f782e9b
Valentinaga1 <1881@holbertonschool.com>
2020-11-26 22:43:23 +0000

    finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
-	if (node->parent->parent->right->left == node)
-		return (node->parent->parent->left);
-	return (node->parent->parent->right);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
