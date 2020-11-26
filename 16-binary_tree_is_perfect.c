6dc0a39ebaf8bbfbf8676c36afd37f83c6b73ab6
Victor Zuluaga <zrvictor@outlook.com>
2020-11-26 11:10:49 -0500

    check whether binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,19 +7,28 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int lheight = 0;
-	int rheight = 0;
+	int level = 0;
 
-	if (!tree)
-		return (0);
+	level = binary_tree_height(tree);
+	return ((size_t)_pow_recursion(2, level) - 1 == binary_tree_size(tree));
+}
 
-	lheight = binary_tree_height(tree->left);
-	rheight = binary_tree_height(tree->right);
+/**
+ * _pow_recursion - number to the power
+ * @x: Number to power
+ * @y: number to indicate number times.
+ *
+ * Return: Number to the Power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
 
-	if (lheight == rheight)
+	if (y == 0)
 		return (1);
-	else
-		return (0);
+
+	return (_pow_recursion(x, y - 1) * x);
 }
 
 /**
@@ -33,7 +42,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int lheight = 0;
 	int rheight = 0;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
 
 	lheight = binary_tree_height(tree->left);
@@ -43,4 +52,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (lheight + 1);
 	else
 		return (rheight + 1);
+}
+
+/**
+ *  binary_tree_size - calculate the size of the bt
+ * @tree: Pointer  to the root node of the tree to traverse
+ *
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) +
+			1 + binary_tree_size(tree->right));
 }
