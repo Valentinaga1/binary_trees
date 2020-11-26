4f7f9fa6d306e7bc7f1f852af152051992a506fc
Victor Zuluaga <zrvictor@outlook.com>
2020-11-26 14:44:46 -0500

    null case

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,13 +9,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
-	if (node->parent->parent->left == NULL && node->parent->parent->left)
-		return (NULL);
-
 	if (node->parent->parent->right->left == node)
 		return (node->parent->parent->left);
 	else if (node->parent->parent->right->right == node)
 		return (node->parent->parent->left);
 	else
 		return (node->parent->parent->right);
+
+	return (NULL);
 }
